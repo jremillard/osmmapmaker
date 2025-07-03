0ba07fa6400756261ef6376ef181c0dc7cf0b3fc
Jason Remillard <remillard.jason@gmail.com>
2025-07-03 17:15:10 -0400

    Update vcpkg dependencies

--- vcpkg-setup.bat
+++ vcpkg-setup.bat
@@ -1,10 +1,9 @@
 cd vcpkg
 
-vcpkg install mapnik[cairo,sqlite3] --triplet x64-windows
-
-rem vcpkg install geos libosmium sqlitecpp qt5-tools qt5-imageformats qt5-location --triplet x64-windows
-
-
+rem Install all libraries required by the CMake build files
+vcpkg install qt5-base qt5-tools qt5-xmlpatterns ^
+    bzip2 zlib expat proj4 sqlite3 sqlitecpp geos libosmium catch2 ^
+    --triplet x64-windows
 
 cd ..
 
