36efc4461f66864b1b5217a9683a4d80396b7e98
Jason Remillard <remillard.jason@gmail.com>
2025-07-04 18:07:20 -0400

    Add OSM sample dataset and validation

--- tests/osmdata_test.cpp
+++ tests/osmdata_test.cpp
@@ -3,6 +3,10 @@
 #include "osmdatadirectdownload.h"
 #include "osmdataextractdownload.h"
 #include <QtXml>
+#include <QXmlSchema>
+#include <QXmlSchemaValidator>
+#include <QDir>
+#include <QNetworkAccessManager>
 #include <SQLiteCpp/SQLiteCpp.h>
 #include "renderdatabase.h"
 
@@ -59,6 +63,7 @@ TEST_CASE("OsmDataFile XML round trip", "[OsmDataFile]")
 
 TEST_CASE("OsmData importFile inserts entities", "[OsmData]")
 {
+    QString baseDir = QStringLiteral(SOURCE_DIR);
     RenderDatabase rdb;
     SQLite::Database& db = rdb.db();
 
@@ -157,4 +162,53 @@ TEST_CASE("OsmDataExtractDownload saveXML", "[OsmData]")
 
     REQUIRE(outElem.tagName() == "openStreetMapExtractDownload");
     REQUIRE(outElem.firstChildElement("dataSource").text() == "OSM");
+}
+
+TEST_CASE("Rendering sample OSM files import", "[OsmData]")
+{
+    QString baseDir = QStringLiteral(SOURCE_DIR);
+    QDir dir(baseDir + "/tests/osm/render_cases");
+    QStringList files = dir.entryList(QStringList() << "case*.osm", QDir::Files);
+    REQUIRE(files.size() == 30);
+
+    for (const QString& fileName : files) {
+        RenderDatabase rdb;
+        SQLite::Database& db = rdb.db();
+
+        OsmDataFile data;
+        data.SetLocalFile(dir.filePath(fileName));
+        data.importData(db);
+
+        SQLite::Statement countStmt(db, "SELECT COUNT(*) FROM entity");
+        REQUIRE(countStmt.executeStep());
+        REQUIRE(countStmt.getColumn(0).getInt() > 0);
+    }
+}
+
+TEST_CASE("Rendering sample OSM schema validation", "[OsmSchema]")
+{
+    int argc = 0;
+    qputenv("QT_PLUGIN_PATH", "");
+    QCoreApplication app(argc, nullptr);
+    QCoreApplication::setLibraryPaths(QStringList());
+    QNetworkAccessManager nam;
+    nam.setNetworkAccessible(QNetworkAccessManager::NotAccessible);
+
+    QXmlSchema schema;
+    QFile xsdFile(QStringLiteral(SOURCE_DIR "/tests/osm/osm_test.xsd"));
+    REQUIRE(xsdFile.open(QIODevice::ReadOnly));
+    schema.load(&xsdFile);
+    REQUIRE(schema.isValid());
+
+    QDir dir(QStringLiteral(SOURCE_DIR "/tests/osm/render_cases"));
+    QStringList files = dir.entryList(QStringList() << "case*.osm", QDir::Files);
+    for (const QString& f : files) {
+        QFile dataFile(dir.filePath(f));
+        REQUIRE(dataFile.open(QIODevice::ReadOnly));
+        QXmlSchemaValidator validator(schema);
+        REQUIRE(validator.validate(&dataFile));
+    }
+    schema = QXmlSchema();
+    app.processEvents();
+    QCoreApplication::sendPostedEvents(nullptr, QEvent::DeferredDelete);
 }
