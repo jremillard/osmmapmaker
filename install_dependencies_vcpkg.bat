2b4c332f6af20a047dd3fa7f66093efa0671122e
Jason Remillard <remillard.jason@gmail.com>
2025-07-06 01:16:14 -0400

    Rename and update vcpkg setup script

--- install_dependencies_vcpkg.bat
+++ install_dependencies_vcpkg.bat
@@ -1,4 +1,5 @@
 @echo off
+REM The Visual Studio environment must be set up in this command shell first.
 REM Navigate into the vcpkg directory
 cd vcpkg
 
@@ -10,7 +11,7 @@ git checkout 2025-06-02
 
 REM Install all libraries required by the CMake build files
 vcpkg install qt5-base qt5-tools qt5-xmlpatterns ^
-    bzip2 zlib expat proj4 sqlite3 sqlitecpp geos libosmium catch2 ^
+    bzip2 zlib expat proj4 sqlite3 sqlitecpp geos libosmium catch2 spdlog ^
     --triplet x64-windows
 
 cd ..
